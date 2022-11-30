1fa90b936ac030f59e3b60b71ab375a23069f1ed
DanielSparza <danielesparzza@gmail.com>
2022-11-30 15:04:37 -0600

    Se eliminaron metodos no usados en comunidadController

--- app/Http/Controllers/ComunidadController.php
+++ app/Http/Controllers/ComunidadController.php
@@ -32,16 +32,6 @@ class ComunidadController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -70,17 +60,6 @@ class ComunidadController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -96,16 +75,5 @@ class ComunidadController extends Controller
 
         $comunidad->save();
         return $comunidad;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
